5cb83191c93f34f8fa8efe2b68682a68281601c7
ElenKr <87223078+ElenKr@users.noreply.github.com>
2021-07-14 11:38:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
+	
+    am = document.getElementById("principal").value; //amount
+    i = document.getElementById("rate").value; // interest
+    y = document.getElementById("years").value; // years
+    var val=(am*i*y)/100;
+    var p = document.createElement('p');
+  	document.body.appendChild(p);
+  	p.textContent='If you deposit ' + am +' at an interest ' +i +'%.'+' You will receive an amount of '+ val;
         
